ae73af20f2770c3f8bb35a4683d92b93675ecd7e
dhythm <yuta.okada.20@gmail.com>
2022-10-22 01:35:44 +0900

    feat(app): app works woth simplebar-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
 import "./App.css";
 import { ComponentWithSimpleBar } from "./ComponentWithSimpleBar";
+import reactLogo from "./assets/react.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <ComponentWithSimpleBar />
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
       <h1>Vite + React</h1>
+      <ComponentWithSimpleBar />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- src/ComponentWithSimpleBar.tsx
+++ src/ComponentWithSimpleBar.tsx
@@ -1,19 +1,17 @@
 import SimpleBar from "simplebar-react";
-// import "simplebar-react/dist/simplebar.min.css";
-import reactLogo from "./assets/react.svg";
+// import "simplebar";
+// import "simplebar/dist/simplebar.css";
+import "simplebar-react/dist/simplebar.min.css";
 import "./ComponentWithSimpleBar.css";
 
 export const ComponentWithSimpleBar = () => {
   return (
     <SimpleBar style={{ maxHeight: 150, overflow: "auto" }}>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      {[...Array(50)].map((x, i) => (
+        <p key={i} className="odd">
+          Some content
+        </p>
+      ))}
     </SimpleBar>
   );
 };
