786c26b24e7f98d1897d10643a3927b559e796c7
dhythm <yuta.okada.20@gmail.com>
2022-10-22 23:34:38 +0900

    feat(app): install styled-components and use CSS-in-JS to the component

--- src/ComponentWithSimpleBar.tsx
+++ src/ComponentWithSimpleBar.tsx
@@ -3,20 +3,27 @@ import SimpleBar from "simplebar-react";
 // import "simplebar";
 // import "simplebar/dist/simplebar.css";
 import "simplebar-react/dist/simplebar.min.css";
-import "./ComponentWithSimpleBar.css";
+import styled from "styled-components";
+// import "./ComponentWithSimpleBar.css";
 
 type Props = {
   children: ReactNode;
 };
 export const ComponentWithSimpleBar: FC<Props> = ({ children }) => {
   return (
-    <SimpleBar style={{ maxHeight: 150, overflow: "auto" }}>
+    <StyledSimpleBar style={{ maxHeight: 150, overflow: "auto" }}>
       {/* {[...Array(50)].map((x, i) => (
         <p key={i} className="odd">
           Some content
         </p>
       ))} */}
       {children}
-    </SimpleBar>
+    </StyledSimpleBar>
   );
 };
+
+const StyledSimpleBar = styled(SimpleBar)`
+  & .simplebar-scrollbar::before {
+    background: linear-gradient(darkblue, skyblue);
+  }
+`;
